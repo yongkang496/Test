ce39bb2de9d0450175ea26da47bff4d7e28bb033
yongkanggao <443929830@qq.com>
2018-01-06 14:01:49 +0800

    判断函数类型]

--- test1.py
+++ test1.py
@@ -2,10 +2,20 @@
 __author__ = 'yongkang'
 Date = '2018/1/4'
 
-def my_abs(x):
-    if x >= 0:
-        print(x)
-    else:
-        print(-x)
+# def my_abs(x):
+#     if x >= 0:
+#         print(x)
+#     else:
+#         print(-x)
+#
+# my_abs(-99)
 
-my_abs(-99)
+def my_abs(x):
+    if not isinstance(x,(float,int)):
+        raise TypeError("类型错误")
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+print(my_abs(-99))
