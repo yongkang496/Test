51496689cff3518ee859399ef747375c42a119f4
yongkanggao <443929830@qq.com>
2018-01-06 14:10:36 +0800

    返回多个值

--- test1.py
+++ test1.py
@@ -10,12 +10,21 @@ Date = '2018/1/4'
 #
 # my_abs(-99)
 
-def my_abs(x):
-    if not isinstance(x,(float,int)):
-        raise TypeError("类型错误")
-    if x >= 0:
-        return x
-    else:
-        return -x
+# def my_abs(x):
+#     if not isinstance(x,(float,int)):
+#         raise TypeError("类型错误")
+#     if x >= 0:
+#         return x
+#     else:
+#         return -x
+#
+# print(my_abs(-99))
 
-print(my_abs(-99))
+import math
+
+def move(x,y,step,angle=0):
+    nx = x + step*math.cos(angle)
+    ny = y - step*math.sin(angle)
+    return nx,ny
+
+print(move(100,100,60,math.pi/6))
