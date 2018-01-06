9688dbb0bd8bd41cfa05e6a09839614afd93c957
yongkanggao <443929830@qq.com>
2018-01-06 16:30:53 +0800

    aa

--- test2.py
+++ test2.py
@@ -2,12 +2,20 @@
 __author__ = 'yongkang'
 Date = '2018/1/6'
 
-def power(x,n):
-    s = 1
-    while n > 0:
-        n = n - 1
-        s = s * x
-    return s
+# def power(x,n):
+#     s = 1
+#     while n > 0:
+#         n = n - 1
+#         s = s * x
+#     return s
+#
+# print(power(5,2))
+# print(power(5,3))
 
-print(power(5,2))
-print(power(5,3))
+def calc(numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+
+print(calc([1,2,3]))
