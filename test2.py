e508619797b27ca3f9bfbf536c8621a6166da97f
yongkanggao <443929830@qq.com>
2018-01-07 09:50:57 +0800

    关键字参数

--- test2.py
+++ test2.py
@@ -30,3 +30,26 @@ def calc(*numbers):
     return sum
 
 print(calc(1,2,3))
+
+'''
+关键字参数
+'''
+
+def person(name,age,**kw):
+    print('name:',name,'age:',age,'other:',kw)
+
+exter = {'city':'beijing','job':'engineer'}
+person('paul',30,city = exter['city'],job = exter['job'])
+person('paul',30,**exter)
+
+'''
+命名关键字参数
+'''
+
+def person(name,age,**kw):
+    if 'city' in kw:
+        pass
+    if 'job' in kw:
+        pass
+    print('name:',name,'age:',age,'other:',kw)
+person('jordan',30,city = 'beijing',job = 'farmer')
